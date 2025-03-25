b92e09c33353be6bb9920933608aeec6952dd5f4
wusheng233 <wusheng233@qq.com>
2025-03-25 18:26:30 +0800

    重写chapter1，修复颜色不恢复等bug，更好的开发模式

--- chapter/001.py
+++ chapter/001.py
@@ -1,6 +1,6 @@
 # THIS IS A DEMO VERSION AND NOT ARE THE RELEASE VERSION EXPRIENCE
 # Flandre Studio 20250322
-# Everything by minqwq
+# Copyright (c) 2025 minqwq
 
 import pygame
 import os
@@ -8,255 +8,175 @@ import sys
 import json
 import random
 import time
-import base64
 
-# 防御的计算方式是 "<受伤值> / <防御>"
-# 137 / 1.5 = 91.333333
-
-pygame.mixer.init()
-pygame.mixer.music.load("music/gf01_02.mp3")
-pygame.mixer.music.play()
-
-sfx_select00 = pygame.mixer.Sound("sfx/select00.wav")
-sfx_nep00 = pygame.mixer.Sound("sfx/nep00.wav")
-sfx_ok00 = pygame.mixer.Sound("sfx/ok00.wav")
-sfx_extend = pygame.mixer.Sound("sfx/extend.wav")
-sfx_tan00 = pygame.mixer.Sound("sfx/tan00.wav")
-sfx_pldead00 = pygame.mixer.Sound("sfx/pldead00.wav")
-
-configFile = open("config.json", "r", encoding="utf-8")
-configRead = json.load(configFile)
-overdrive_remain = int(configRead["overdrives"])
-
-score = 0
-overdrive_mode = 0
-spellremain = int(configRead["spells"])
-yk_health = 0
-version = "Preview 0.02a"
-oldknifeisok = True
-devmode = configRead["devmode"]
-devmodepwd = base64.b64decode(b'bGlmZWlzdG91aG91') # lifeistouhou
-seledatk = False
-local_health = 3500
-
-def resetconf():
-    if overdrive_remain > 6:
-        print("config incorrect(> 6)")
-        sys.exit()
-    if spellremain > 6:
-        print("config incorrect(> 6)")
-        sys.exit()
-
-if devmode == "false":
-    resetconf()
-
-if devmode == "true":
-    devmodepwdinput = input("输入开发模式密码 > ")
-    if bytes(devmodepwdinput, encoding="utf-8") == devmodepwd:
-        spellremain = 9999
-        overdrive_remain = 9999
-    else:
-        resetconf()
-
-tiny_youkais = [
-                   "大妖精",
-                   "琪露诺",
-                   "幽灵",
-                   "不保存姐",
-                   "小女仆",
-                   "小妖精"
-               ]
-              
-sel0 = ['''
-def attacking():
-    print("选一个物品吧")
-    print(
-            "1:生锈到快断掉的刀(50 ~ 160)"+
-            "\n2:锤ta(70 ~ 100)(Overdrive mode:150 ~ 270)"+
-            "\n3:脚踢(80 ~ 130)(Overdrive mode:200 ~ 350)"
-         )
-    selatkitem = input("选择编号 >>> ")
-    if selatkitem == "1":
-        if oldknifeisok == False:
-            oldknifebrokentry = random.randint(1, 20)
-            if oldknifebrokentry == "1":
-                print("刀断了...")
-                oldknifeisok = False
+class Chapter1:
+    VERSION = "Preview 0.02a"
+    gameconfig = {}
+    sounds = {}
+    curyoukai = {}
+    gamedir = ""
+    playerhealth = 3500
+    gamescore = 0
+    overdrives = 0
+    overdrivemode = False
+    spells = 0
+    gamedevmode = False
+    tiny_youkais = [
+        {"name": "大妖精", "defend": 1.2, "attack": {"start": 80, "stop": 120}, "health": 2000},
+        {"name": "琪露诺", "defend": 1.09, "attack": {"start": 9, "stop": 99}, "health": 1999},
+        {"name": "幽灵", "defend": 2, "attack": {"start": 1, "stop": 50}, "health": 1000},
+        {"name": "不保存姐", "defend": 1, "attack": 1, "health": 1},
+        {"name": "小女仆", "defend": 1.4, "attack": {"start": 40, "stop": 130}, "health": 1500},
+        {"name": "小妖精", "defend": 1.1, "attack": {"start": 40, "stop": 60}, "health": 1000}
+    ]
+    weapons = [
+        {"name": "生锈到快断掉的刀", "damage": {"start": 50, "stop": 160}, "overdrive": False, "broken": {"stop": 20, "msg": "刀断了..."}},
+        {"name": "锤ta", "damage": {"start": 70, "stop": 100}, "overdrive": {"start": 150, "stop": 270}, "broken": False},
+        {"name": "脚踢", "damage": {"start": 80, "stop": 130}, "overdrive": {"start": 200, "stop": 350}, "broken": False},
+    ]
+    def __init__(self):
+        self.gamedir = os.path.dirname(os.path.abspath(__file__)) + os.sep
+        self.gameconfig = json.load(open(self.gamedir + "config.json", "r", encoding="utf-8"))
+        self.overdrives = int(self.gameconfig["overdrives"])
+        self.spells = int(self.gameconfig["spells"]) # TODO: 修复类型错误
+        self.gamedevmode = self.gameconfig["devmode"] == "true"
+        if self.gamedevmode:
+            self.weapons.append({"name": "test", "damage": 10, "overdrive": 20, "broken": False})
+            self.weapons.append({"name": "brokentest", "damage": 1, "overdrive": 2, "broken": {"stop": 2, "msg": "OK"}})
+            self.weapons.append({"name": "一刀9999", "damage": 9999, "overdrive": 99999, "broken": False})
+            self.weapons.append({"name": "testNoOverdriveNoBroken", "damage": 68, "overdrive": False, "broken": False})
+        pygame.mixer.init()
+        pygame.mixer.music.load("music/gf01_02.mp3")
+        pygame.mixer.music.play()
+        self.loadSound("select00", "sfx/select00.wav")
+        self.loadSound("nep00", "sfx/nep00.wav")
+        self.loadSound("ok00", "sfx/ok00.wav")
+        self.loadSound("extend", "sfx/extend.wav")
+        self.loadSound("tan00", "sfx/tan00.wav")
+        self.loadSound("pldead00", "sfx/pldead00.wav")
+        self.randomChoice()
+        self.mainLoop()
+    def loadSound(self, name, path):
+        self.sounds[name] = pygame.mixer.Sound(path)
+    def playSound(self, name):
+        self.sounds[name].play()
+    def mainLoop(self):
+        clearScreen()
+        if self.curyoukai["health"] <= 0: # FIXME: 有的时候血量变负值都没有触发死亡
+            self.playSound("pldead00")
+            self.playerhealth += self.curyoukai["health"] / 8 + 350;
+            self.randomChoice()
+        if self.playerhealth <= 0:
+            self.playSound("pldead00")
+            print("你输了")
+            time.sleep(3)
+            return
+        if self.overdrivemode:
+            print("\033[91m")
+        else:
+            print("\033[0m")
+        print("(这是水印)这是一个体验版本，不代表做完后的样子。")
+        print("版本 " + self.VERSION)
+        print()
+        print("当前妖怪名字: " + self.curyoukai["name"])
+        print("防 " + str(self.curyoukai["defend"]) + ", 攻 " + str(self.curyoukai["attack"]) + ", 对方当前血量: " + str(self.curyoukai["health"]))
+        print("分数 " + str(round(self.gamescore)) + ", 可再超载 " + str(self.overdrives) + " 次, 还有 " + str(self.spells) + " 个符卡")
+        print("自身血量 " + str(self.playerhealth))
+        print("1:攻击")
+        print("2:bomb")
+        print("3:我不玩了")
+        print("4:超载机能")
+        if self.gamedevmode:
+            print("5:添加100个符卡")
+            print("6:下一个对手")
+            print("7:清空自身血量")
+        choice = input("选择一个选项然后继续 >>> \033[0m").strip()
+        if self.overdrivemode:
+            print("\033[91m")
+        if choice == "1":
+            self.playSound("select00")
+            for i, weapon in enumerate(self.weapons):
+                print(str(i + 1) + 
+                    ":" + 
+                    weapon["name"] + 
+                    "(" + 
+                    (
+                        str(weapon["damage"]) if isinstance(weapon["damage"], int) else (
+                            str(weapon["damage"]["start"]) + 
+                            " ~ " + 
+                            str(weapon["damage"]["stop"])
+                        )
+                    ) + 
+                    ")" + 
+                    (
+                        (
+                            "(超载模式:" + 
+                            (
+                                (
+                                    str(weapon["overdrive"]["start"]) + 
+                                    " ~ " + 
+                                    str(weapon["overdrive"]["stop"])
+                                ) if isinstance(weapon["overdrive"], dict) else str(weapon["overdrive"])
+                            ) + 
+                            ")"
+                        ) if weapon["overdrive"] != False else ""
+                    )
+                )
+            try:
+                choice = int(input("选择编号 >>> ").strip()) - 1
+            except ValueError:
+                print("输入错误")
+                time.sleep(2)
+            else:
+                self.playSound("ok00")
+                if len(self.weapons) > choice:
+                    weapon = self.weapons[choice]
+                    if weapon["broken"] != False and random.randint(1, weapon["broken"]["stop"]) == 1:
+                        print(weapon["broken"]["msg"])
+                        time.sleep(1)
+                    else:    
+                        damage = weapon["damage"] if (weapon["overdrive"] != False and self.overdrivemode) != True else weapon["overdrive"]
+                        if isinstance(damage, dict):
+                            damage = random.randint(damage["start"], damage["stop"])
+                        self.curyoukai["health"] -= damage
+                        print("对方受到伤害 " + str(damage))
+                        self.playSound("tan00")
+                        time.sleep(1)
+                        self.gamescore += random.randint(20000, 45000) * self.curyoukai["defend"] * damage / 50
+                        self.playerhealth -= self.curyoukai["attack"]
+        elif choice == "2": # 这个选项单独做出来不知道是什么意思
+            self.playSound("ok00")
+            if self.spells <= 0:
+                print("符卡不足")
                 time.sleep(1)
             else:
-                stdatk = random.randint(50, 160)
-                yk_health -= stdatk
-                print("对方受到伤害 " + stdatk)
-                sfx_tan00.play()
+                print("奇迹「七彩大炮」！！")
                 time.sleep(1)
-        else:
-            print("断了，不能用刀把。")
-            time.sleep(1)
-    elif selatkitem == "2":
-        if overdrive_mode >= 1:
-            stdatk = random.randint(150, 270)
-            yk_health -= stdatk
-            print("对方受到伤害 " + stdatk)
-            sfx_tan00.play()
-            time.sleep(1)
-        else:
-            stdatk = random.randint(70, 100)
-            yk_health -= stdatk
-            print("对方受到伤害 " + stdatk)
-            sfx_tan00.play()
-            time.sleep(1)
-    elif selatkitem == "3":
-        if overdrive_mode >= 1:
-            stdatk = random.randint(200, 350)
-            yk_health -= stdatk
-            print("对方受到伤害 " + stdatk)
-            sfx_tan00.play()
-            time.sleep(1)
-        else:
-            stdatk = random.randint(80, 130)
-            yk_health -= stdatk
-            print("对方受到伤害 " + stdatk)
-            sfx_tan00.play()
-            time.sleep(1)
-''']
-
-randomchoice = '''spellatklo = 350
-spellatkhi = 700
-
-curyoukainame = random.choice(tiny_youkais)
-if curyoukainame == "大妖精":
-    yk_defend = 1.2
-    yk_attack = random.randint(80, 120)
-    yk_health = 2000
-elif curyoukainame == "琪露诺":
-    yk_defend = 1.09
-    yk_attack = random.randint(9, 99)
-    yk_health = 1999
-elif curyoukainame == "幽灵":
-    yk_defend = 2
-    yk_attack = random.randint(1, 50)
-    yk_health = 1000
-elif curyoukainame == "不保存姐":
-    yk_defend = 1
-    yk_attack = 1
-    yk_health = 1
-elif curyoukainame == "小女仆":
-    yk_defend = 1.4
-    yk_attack = random.randint(40, 130)
-    yk_health = 1500
-elif curyoukainame == "小妖精":
-    yk_defend = 1.1
-    yk_attack = random.randint(40, 60)
-    yk_health = 1000'''
-exec(randomchoice)    
-
-while True:
-    if overdrive_mode >= 1:
-        print("\033[31m")
-    elif overdrive_mode <= 0:
-        print("\033[0m")
-    if yk_health <= 0:
-        sfx_pldead00.play()
-        local_health += yk_health / 8 + 350
-        exec(randomchoice)  
-    clearScreen()
-    print("(这是水印)这是一个体验版本，不代表做完后的样子。\n版本 " + version + "\n")
-    print("当前妖怪名字: " + curyoukainame)
-    print("防 " + str(yk_defend) + ", 攻 " + str(yk_attack) + ", 对方当前血量: " + str(yk_health))
-    print("分数 " + str(round(score)) + ", 可再超载 " + str(overdrive_remain) + " 次, 还有 " + str(spellremain) + " 个符卡")
-    print("自身血量 " + str(local_health))
-    if overdrive_mode >= 1:
-        print("超载模式还能再维持 " + str(overdrive_mode) + " 回")
-    ykatk = input("1:攻击\n2:bomb\n3:我不玩了\n4:超载机能\n选择一个选项然后继续 >>> ")
-    if ykatk == "1":
-        sfx_select00.play()
-        print("选一个物品吧")
-        print(
-                "1:生锈到快断掉的刀(50 ~ 160)"+
-                "\n2:锤ta(70 ~ 100)(Overdrive mode:150 ~ 270)"+
-                "\n3:脚踢(80 ~ 130)(Overdrive mode:200 ~ 350)"
-             )
-        selatkitem = input("选择编号 >>> ")
-        if selatkitem == "1":
-            sfx_ok00.play()
-            oldknifebrokentry = random.randint(1, 20)
-            if oldknifebrokentry == "1":
-                print("刀断了...")
-                oldknifeisok = False
+                self.playSound("nep00")
+                damage = random.randint(350, 700)
+                self.curyoukai["health"] -= damage
+                print("对方受到伤害 " + str(damage))
                 time.sleep(1)
-            else:
-                stdatk = random.randint(50, 160)
-                yk_health -= stdatk
-                print("对方受到伤害 " + str(stdatk))
-                sfx_tan00.play()
-                time.sleep(1)
-                seledatk = True
-        elif selatkitem == "2":
-            sfx_ok00.play()
-            if overdrive_mode > 0:
-                stdatk = random.randint(150, 270)
-                yk_health -= stdatk
-                print("对方受到伤害 " + str(stdatk))
-                sfx_tan00.play()
-                time.sleep(1)
-                overdrive_mode -= 1
-                seledatk = True
-            else:
-                stdatk = random.randint(70, 100)
-                yk_health -= stdatk
-                print("对方受到伤害 " + str(stdatk))
-                sfx_tan00.play()
-                time.sleep(1)
-                seledatk = True
-        elif selatkitem == "3":
-            sfx_ok00.play()
-            if overdrive_mode > 0:
-                stdatk = random.randint(200, 350)
-                yk_health -= stdatk
-                print("对方受到伤害 " + str(stdatk))
-                sfx_tan00.play()
-                overdrive_mode -= 1
-                time.sleep(1)
-                seledatk = True
-            else:
-                stdatk = random.randint(80, 130)
-                yk_health -= stdatk
-                print("对方受到伤害 " + str(stdatk))
-                sfx_tan00.play()
-                time.sleep(1)
-                seledatk = True
-        else:
-            pass
-        if seledatk == True:
-            score += random.randint(20000, 45000) * yk_defend * stdatk / 50
-            local_health -= yk_attack
-            seledatk = False
-        elif seledatk == False:
-            pass
-    elif ykatk == "2":
-        sfx_ok00.play()
-        if spellremain <= 0:
-            print("符卡不足")
-            time.sleep(1)
-            continue
-        print("奇迹「七彩大炮」！！")
-        time.sleep(1)
-        sfx_nep00.play()
-        spellatk = random.randint(spellatklo, spellatkhi)
-        yk_health -= spellatk
-        print("对方受到伤害 " + str(spellatk))
-        time.sleep(1)
-        score += random.randint(20000, 45000) * yk_defend
-        spellremain -= 1
-    elif ykatk == "3":
-        sys.exit()
-    elif ykatk == "4":
-        if overdrive_remain >= 0 and overdrive_mode <= 0:
-            overdrive_mode = 5
-            overdrive_remain -= 1
-            print("已启用超载模式。")
-            sfx_extend.play()
-            time.sleep(1)
-        else:
-            print("没用。(可用为0或仍处于超载模式中)")
-            time.sleep(1)
+                self.gamescore += random.randint(20000, 45000) * self.curyoukai["defend"]
+                self.spells -= 1
+        elif choice == "3":
+            sys.exit(0)
+        elif choice == "4":
+            self.overdrives -= 1
+            self.overdrivemode = True
+            self.playSound("extend")
+        elif self.gamedevmode:
+            if choice == "5":
+                self.spells += 100
+            elif choice == "6":
+                self.randomChoice()
+            elif choice == "7":
+                self.playerhealth = 0
+        if self.gamedevmode:
+            input("按下Enter继续主循环")
+        self.mainLoop()
+    def randomChoice(self):
+        self.curyoukai = random.choice(self.tiny_youkais) # TODO: 随机防御值生命值
+        if isinstance(self.curyoukai["attack"], dict):
+            self.curyoukai["attack"] = random.randint(self.curyoukai["attack"]["start"], self.curyoukai["attack"]["stop"])
+Chapter1()
